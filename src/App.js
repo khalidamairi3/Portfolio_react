fa2469ba01a7688cebe2670aae371c4f7fd489b9
Khaled Amayri <khalidamairi3@gmail.com>
2023-06-06 21:07:52 -0600

    header with name completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 
 import * as React from 'react';
+import Header from './components/Header';
 import Button from '@mui/material/Button';
 
 export default function App() {
   return (
     <div>
-      <Button variant="contained">Hello World</Button>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+
+const Header = () => {
+    return (
+
+        <AppBar position="stitc">
+            <Toolbar><Typography variant="h6">
+                Meera
+            </Typography></Toolbar>
+        </AppBar>
+
+    );
+}
+
+
+export default Header;
